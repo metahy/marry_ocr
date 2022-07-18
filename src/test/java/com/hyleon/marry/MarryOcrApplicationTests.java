9ff29625b9f447ba639a3edc60b29f2568ae549e
Hyleon Tsao <hyleontsao@gmail.com>
2022-07-18 18:18:15 +0800

    Initial commit

---
+++ src/test/java/com/hyleon/marry/MarryOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyleon.marry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
